f5f0395a0fc108eccb4363e7f96f25e12866a1ff
VPudi <VPudi@users.noreply.github.com>
2018-05-07 21:51:35 -0400

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,34 @@
+## Downloaded the data file to local folder manually.
+
+##set working directory in R to the above local folder
+
+setwd("C:\Coursera\Exploratory Graphs\Project1")
+
+
+## Read the entire file
+
+hpc <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", nrows=-1)
+
+
+## subset the dataset for Feb 1st and 2nd of 2007 data
+
+hpc2 <- subset(hpc, Date=="1/2/2007" | Date=="2/2/2007")
+
+## Create a new 10th column as DateTime Column by combining Date and Time
+
+hpc2[,10] <- as.POSIXct(paste(hpc2$Date, hpc2$Time), format="%d/%m/%Y %H:%M:%S")
+names(hpc2)[10]="DateTime"
+
+
+## set device to png
+
+png("Plot1.png")
+
+## generate the histogram for Plot 1
+
+hist(hpc2$Global_active_power, breaks=25, col="red", main="Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+##close the png 
+
+dev.off()
+
---
+++ Plot4.R
@@ -0,0 +1,42 @@
+## Downloaded the data file to local folder manually.
+
+##set working directory in R to the above local folder
+
+setwd("C:\Coursera\Exploratory Graphs\Project1");
+
+
+## Read the entire file
+
+hpc <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", nrows=-1);
+
+
+## subset the dataset for Feb 1st and 2nd of 2007 data
+
+hpc2 <- subset(hpc, Date=="1/2/2007" | Date=="2/2/2007");
+
+## Create a new 10th column as DateTime Column by combining Date and Time
+
+hpc2[,10] <- as.POSIXct(paste(hpc2$Date, hpc2$Time), format="%d/%m/%Y %H:%M:%S");
+names(hpc2)[10]="DateTime"
+
+
+## set device to png
+
+png("Plot4.png");
+
+## generate the 4 Plots
+
+par(mfcol=c(2,2));
+plot(hpc2$DateTime, hpc2$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)");
+plot(hpc2$DateTime, hpc2$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering");
+lines(hpc2$DateTime, hpc2$Sub_metering_2, col = "red");
+lines(hpc2$DateTime, hpc2$Sub_metering_3, col = "blue");
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1), lwd=1, col=c("black","red","blue"));
+with (hpc2, plot(DateTime, Voltage, type="l"));
+with (hpc2, plot(DateTime, Global_reactive_power, type="l"));
+
+
+##close the png 
+
+dev.off();
+
